Fixed bugs in unit conversion and rounding with integer types

src/dspeed/processing_chain.py
@@ -24,6 +24,7 @@ from pint import Quantity, Unit
 
 from .errors import DSPFatal, ProcessingChainError
 from .units import unit_registry as ureg
+from .processors.round_to_nearest import round_to_nearest
 from .utils import ProcChainVarBase
 
 log = logging.getLogger(__name__)
@@ -967,24 +968,32 @@ class ProcessingChain:
                     grid = CoordinateGrid(to_nearest, var.grid.offset)
                 else:
                     grid = to_nearest
-                conversion_manager = UnitConversionManager(var, grid, round=True)
-            else:
-                grid = var.grid
-                if isinstance(to_nearest, (int, float)):
-                    to_nearest = to_nearest * var.unit
-                conversion_manager = UnitConversionManager(var, to_nearest, round=True)
 
-            out = ProcChainVar(
-                var.proc_chain,
-                name,
-                var.shape,
-                dtype,
-                grid,
-                var.unit,
-                var.is_coord,
-            )
-            out._buffer = conversion_manager.out_buffer
-            var.proc_chain._proc_managers.append(conversion_manager)
+                out = ProcChainVar(
+                    var.proc_chain,
+                    name,
+                    var.shape,
+                    dtype,
+                    grid,
+                    var.unit,
+                    var.is_coord,
+                )
+                conversion_manager = UnitConversionManager(var, grid, round=True)
+                out._buffer = conversion_manager.out_buffer
+                var.proc_chain._proc_managers.append(conversion_manager)
+            else:
+                out = ProcChainVar(
+                    var.proc_chain,
+                    name,
+                    var.shape,
+                    dtype,
+                    var.grid,
+                    var.unit,
+                    var.is_coord,
+                )
+
+                var.proc_chain.add_processor(round_to_nearest, var, to_nearest, out)
+
             return out
 
     # type cast variable
@@ -1324,22 +1333,26 @@ class ProcessorManager:
 class UnitConversionManager(ProcessorManager):
     """A special processor manager for handling converting variables between unit systems."""
 
-    @vectorize(nopython=True, cache=True)
+    @vectorize([f"{t}({t}, f8, f8, f8)" for t in ["f4", "f8"]],
+               nopython=True, cache=True
+               )
     def convert(buf_in, offset_in, offset_out, period_ratio):  # noqa: N805
         return (buf_in + offset_in) * period_ratio - offset_out
 
-    @vectorize(nopython=True, cache=True)
+    @vectorize([f"{t}({t}, f8, f8, f8)" for t in ["u1", "u2", "u4", "u8", "i1", "i2", "i4", "i8"]],
+               nopython=True, cache=True
+               )
     def convert_int(buf_in, offset_in, offset_out, period_ratio):  # noqa: N805
         tmp = (buf_in + offset_in) * period_ratio - offset_out
-        ret = round(tmp)
+        ret = np.rint(tmp)
         if np.abs(tmp - ret) < 1.0e-5:
             return ret
         else:
-            raise DSPFatal("Cannot convert to integer")
+            raise DSPFatal("Cannot convert to integer. Use round or astype")
 
-    @vectorize(nopython=True, cache=True)
+    @vectorize([f"{t}({t}, f8, f8, f8)" for t in ["u1", "u2", "u4", "u8", "i1", "i2", "i4", "i8", "f4", "f8"]], nopython=True, cache=True)
     def convert_round(buf_in, offset_in, offset_out, period_ratio):  # noqa: N805
-        return np.round((buf_in + offset_in) * period_ratio - offset_out)
+        return np.rint((buf_in + offset_in) * period_ratio - offset_out)
 
     def __init__(
         self,
@@ -1352,10 +1365,11 @@ class UnitConversionManager(ProcessorManager):
         # callable function used to process data
         if round:
             self.processor = UnitConversionManager.convert_round
-        elif np.issubdtype(var.dtype, np.integer):
-            self.processor = UnitConversionManager.convert_int
-        else:
+        elif issubclass(var.dtype.type, np.floating):
             self.processor = UnitConversionManager.convert
+        else:
+            self.processor = UnitConversionManager.convert_int
+
         # list of parameters prior to converting to internal representation
         self.params = [var, unit]
         self.kw_params = {}
@@ -1365,14 +1379,24 @@ class UnitConversionManager(ProcessorManager):
             to_offset = unit.get_offset()
             unit = unit.period
 
-        from_buffer, from_unit = var._buffer[0]
+        if isinstance(var._buffer, list):
+            from_buffer, from_unit = var._buffer[0]
+        else:
+            from_buffer = var._buffer
+            from_unit = var.unit
+            if isinstance(from_unit, str) and from_unit in ureg:
+                from_unit = ureg.Quantity(from_unit)
+
         if isinstance(from_unit, CoordinateGrid):
             ratio = from_unit.get_period(unit)
             from_offset = from_unit.get_offset()
-        elif isinstance(from_unit, (str, Unit, Quantity)):
+        elif isinstance(from_unit, (Unit, Quantity)):
             if isinstance(unit, str):
                 unit = ureg.Quantity(unit)
             ratio = float(from_unit / unit)
+            from_offset = 0
+        else:
+            ratio = 1/unit
             from_offset = 0
 
         self.out_buffer = np.zeros_like(from_buffer, dtype=var.dtype)