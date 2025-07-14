357d505dd78b17bfa19c2d4cda41de247b58d4bb
iguinn <guinnis@ornl.gov>
2025-07-13 20:21:14 -0700

    Improved round_to_nearest tests

--- tests/processors/test_round_to_nearest.py
+++ tests/processors/test_round_to_nearest.py
@@ -4,6 +4,7 @@ from dspeed.processors import (
     ceil_to_nearest,
     floor_to_nearest,
     round_to_nearest,
+    trunc_to_nearest
 )
 
 
@@ -24,3 +25,15 @@ def test_round_to_nearest(compare_numba_vs_python):
         compare_numba_vs_python(ceil_to_nearest, w_in, 1.5)
         == np.array([-1.5, -1.5, 0.0, 0.0, 0.0, 0.0, 1.5, 1.5, 1.5, 1.5, 3.0])
     )
+
+    assert np.all(
+        compare_numba_vs_python(trunc_to_nearest, w_in, 1.5)
+        == np.array([-1.5, -1.5, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.5, 1.5])
+    )
+
+    w_in = np.nan
+    assert np.isnan(compare_numba_vs_python(round_to_nearest, w_in, 1.5))
+    assert np.isnan(compare_numba_vs_python(floor_to_nearest, w_in, 1.5))
+    assert np.isnan(compare_numba_vs_python(ceil_to_nearest, w_in, 1.5))
+    assert np.isnan(compare_numba_vs_python(trunc_to_nearest, w_in, 1.5))
+
