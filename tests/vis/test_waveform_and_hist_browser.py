46d5a86295e6ae0f5b1454f76a77df97f75f57bb
Schwarz, Mario <schmar9@gmx.de>
2025-08-21 15:54:53 +0200

    tests for the WaveformAndHistBrowser

---
+++ tests/vis/test_waveform_and_hist_browser.py
@@ -0,0 +1,42 @@
+from pathlib import Path
+
+from dspeed.vis import WaveformAndHistBrowser
+
+config_dir = Path(__file__).parent / "configs"
+
+
+def test_basics(lgnd_test_data):
+    wb = WaveformAndHistBrowser(
+        lgnd_test_data.get_path("lh5/LDQTA_r117_20200110T105115Z_cal_geds_raw.lh5"),
+        "/geds/raw",
+        dsp_config=f"{config_dir}/hpge-dsp-histo-config.yaml",
+        lines=["waveform", "wf_mode"],
+        styles="seaborn-v0.8",
+        hist_values_edges=("wf_hist", "wf_borders"),
+        hist_styles=[
+            {"color": ["red","green"]}
+        ],
+    )
+
+    wb.draw_next()
+    wb.draw_entry(24)
+    wb.draw_entry([2, 24])
+
+def test_solo_and_log(lgnd_test_data):
+    wb = WaveformAndHistBrowser(
+        lgnd_test_data.get_path("lh5/LDQTA_r117_20200110T105115Z_cal_geds_raw.lh5"),
+        "/geds/raw",
+        dsp_config=f"{config_dir}/hpge-dsp-histo-config.yaml",
+        lines=[],
+        hist_values_edges=("wf_hist", "wf_borders"),
+        hist_styles=[
+            {"color": ["red","green"]}
+        ],
+        hist_log=True,
+        vertical_hist=True
+    )
+
+    wb.draw_next()
+    wb.draw_entry(24)
+    wb.draw_entry([2, 24])
+
