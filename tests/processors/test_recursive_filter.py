8b3b321b44d682ac9afa1f2e76dd505f2b21b85d
iguinn <guinnis@ornl.gov>
2025-09-23 14:00:50 -0700

    Renamed transfer_function_convolver to recursive_filter, improved docstring, and modified to work with scipy generated iir filters

--- tests/processors/test_recursive_filter.py
+++ tests/processors/test_recursive_filter.py
@@ -2,12 +2,12 @@ import numpy as np
 import pytest
 
 from dspeed.errors import DSPFatal
-from dspeed.processors import pole_zero, transfer_function_convolver
+from dspeed.processors import pole_zero, recursive_filter
 
 
-def test_transfer_function_convolver(compare_numba_vs_python):
+def test_recursive_filter(compare_numba_vs_python):
     """
-    Testing function for the transfer_function_convolver processor.
+    Testing function for the recursive_filter processor.
     Test that this function reproduces a single pole zero filter
 
     """
@@ -16,32 +16,31 @@ def test_transfer_function_convolver(compare_numba_vs_python):
     tau = 10
     ts = np.arange(0, 100)
     amplitude = 10
-    pulse_in = np.insert(amplitude * np.exp(-ts / tau), 0, np.zeros(20))
+    w_in = np.insert(amplitude * np.exp(-ts / tau), 0, np.zeros(20))
 
-    a_in = np.array([1, 1], dtype=np.float64)
-    b_in = np.array([1, -np.exp(-1 / tau)], dtype=np.float64)
-    pulse_out = np.zeros(pulse_in.size, dtype=np.float64)
+    a_in = np.array([1, -np.exp(-1 / tau)], dtype=np.float64)
+    b_in = np.array([1, -1], dtype=np.float64)
+    w_out = np.zeros_like(w_in)
 
     # ensure the DSPFatal is raised if the parameter a is a scalar
     with pytest.raises(DSPFatal):
-        transfer_function_convolver(pulse_in, np.array([]), b_in, pulse_out)
+        recursive_filter(w_in, a_in, np.array([]), 0, 0, w_out)
 
     # ensure that if there is a nan in w_in, all nans are outputted
-    w_in = np.ones(pulse_in.size)
     w_in[4] = np.nan
-
     assert np.all(
-        np.isnan(compare_numba_vs_python(transfer_function_convolver, w_in, a_in, b_in))
+        np.isnan(compare_numba_vs_python(recursive_filter, w_in, a_in, b_in, 0, 0))
     )
 
     # ensure that if there is a nan in a_in, all nans are outputted
+    w_in = np.insert(amplitude * np.exp(-ts / tau), 0, np.zeros(20))
     a_wrong_in = np.ones(3)
     a_wrong_in[2] = np.nan
 
     assert np.all(
         np.isnan(
             compare_numba_vs_python(
-                transfer_function_convolver, pulse_in, a_wrong_in, b_in
+                recursive_filter, w_in, a_wrong_in, b_in, 0, 0
             )
         )
     )
@@ -53,15 +52,15 @@ def test_transfer_function_convolver(compare_numba_vs_python):
     assert np.all(
         np.isnan(
             compare_numba_vs_python(
-                transfer_function_convolver, pulse_in, a_in, b_wrong_in
+                recursive_filter, w_in, a_in, b_wrong_in, 0, 0
             )
         )
     )
 
     # ensure that a valid input gives the expected output when comparing the pole-zero correction with the pole-zero processor
-    w_out_expected = compare_numba_vs_python(pole_zero, pulse_in, tau)
+    w_out_expected = compare_numba_vs_python(pole_zero, w_in, tau)
 
     assert np.allclose(
-        compare_numba_vs_python(transfer_function_convolver, pulse_in, a_in, b_in),
+        compare_numba_vs_python(recursive_filter, w_in, a_in, b_in, 0, 0),
         w_out_expected,
     )
