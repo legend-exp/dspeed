4dcdeb4dc18b8ae839184f13b2d8d5b5a210ada5
Ian Guinn <guinnis@ornl.gov>
2025-10-16 18:49:48 -0400

    Update test_wf_correction.py

--- tests/processors/test_wf_correction.py
+++ tests/processors/test_wf_correction.py
@@ -18,7 +18,7 @@ def test_wf_correction(compare_numba_vs_python):
     expected = np.zeros(len_wf, dtype=np.float32)
     w_in[1] = np.nan
     result = compare_numba_vs_python(wf_correction, w_in, w_corr, 0, 2, w_out)[-1]
-    assert np.array_equal(result, expected)
+    assert np.all(np.isnan(result))
 
     # w_corr contains NaN
     w_in = np.ones(len_wf, dtype=np.float32)
