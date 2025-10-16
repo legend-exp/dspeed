1657d60ad3cdcac8ced228f135a36606202837b4
Valerio D'Andrea <valerio.dandrea@lngs.infn.it>
2025-10-16 21:52:44 +0200

    change processor test

--- src/dspeed/processors/wf_correction.py
+++ src/dspeed/processors/wf_correction.py
@@ -12,7 +12,7 @@ from dspeed.utils import numba_defaults_kwargs as nb_kwargs
         "void(float32[:], float32[:], int32, int32, float32[:])",
         "void(float64[:], float64[:], int32, int32, float64[:])",
     ],
-    "(n),(m),(),(),(n)",
+    "(n),(m),(),()->(n)",
     **nb_kwargs,
 )
 def wf_correction(
@@ -60,15 +60,11 @@ def wf_correction(
     if np.isnan(w_corr).any():
         return
 
-    if np.isnan(start_idx):
-        raise DSPFatal("start_idx is nan")
     if start_idx < 0:
         raise DSPFatal("start_idx must be positive")
     if start_idx > len(w_in):
         raise DSPFatal("start_idx must be shorter than input waveform size")
 
-    if np.isnan(stop_idx):
-        raise DSPFatal("stop_idx is nan")
     if stop_idx <= 0:
         raise DSPFatal("stop_idx must be positive")
     if stop_idx > len(w_in):
--- tests/processors/test_wf_correction.py
+++ tests/processors/test_wf_correction.py
@@ -6,55 +6,43 @@ from dspeed.processors import wf_correction
 
 
 def test_wf_correction(compare_numba_vs_python):
-    """Testing function for the inl_correction processor."""
+    """Testing function for the wf_correction processor."""
 
     len_wf = 8
-    len_corr = 2
+    len_corr = 4
 
-    # Case 1: w_in contains NaN
-    w_in = np.ones(len_wf, dtype=np.int32)
+    # w_in contains NaN
+    w_in = np.ones(len_wf, dtype=np.float32)
     w_corr = np.zeros(len_corr, dtype=np.float32)
+    w_out = np.zeros(len_wf, dtype=np.float32)
+    expected = np.zeros(len_wf, dtype=np.float32)
     w_in[1] = np.nan
-    assert np.all(np.isnan(compare_numba_vs_python(wf_correction, w_in, w_corr, 0, 2)))
+    result = compare_numba_vs_python(wf_correction, w_in, w_corr, 0, 2, w_out)[-1]
+    assert np.array_equal(result, expected)
 
-    # Case 2: w_corr contains NaN
-    w_in = np.ones(len_wf, dtype=np.int32)
-    w_corr = np.zeros(len_corr, dtype=np.float32)
+    # w_corr contains NaN
+    w_in = np.ones(len_wf, dtype=np.float32)
     w_corr[1] = np.nan
-    assert np.all(np.isnan(compare_numba_vs_python(wf_correction, w_in, w_corr, 0, 2)))
+    result = compare_numba_vs_python(wf_correction, w_in, w_corr, 0, 2, w_out)[-1]
+    assert np.array_equal(result, expected)
 
-    # Case 3,4,5: start_idx is NaN, negative, larger then w_in length
-    w_in = np.ones(len_wf, dtype=np.int32)
+    # tests on start_idx
     w_corr = np.zeros(len_corr, dtype=np.float32)
-    assert np.all(np.isnan(compare_numba_vs_python(wf_correction, w_in, w_corr, np.nan, 2)))
+    with pytest.raises(DSPFatal):
+        compare_numba_vs_python(wf_correction, w_in, w_corr, -1, 2)[-1]
+    with pytest.raises(DSPFatal):
+        compare_numba_vs_python(wf_correction, w_in, w_corr, 9, 2)[-1]
 
-    w_in = np.ones(len_wf, dtype=np.int32)
-    w_corr = np.zeros(len_corr, dtype=np.float32)
-    assert np.all(np.isnan(compare_numba_vs_python(wf_correction, w_in, w_corr, -1, 2)))
-
-    w_in = np.ones(len_wf, dtype=np.int32)
-    w_corr = np.zeros(len_corr, dtype=np.float32)
-    assert np.all(np.isnan(compare_numba_vs_python(wf_correction, w_in, w_corr, 9, 2)))
-
-    # Case 6,7,8: stop_idx is NaN, negative, larger then w_in length
-    w_in = np.ones(len_wf, dtype=np.int32)
-    w_corr = np.zeros(len_corr, dtype=np.float32)
-    assert np.all(np.isnan(compare_numba_vs_python(wf_correction, w_in, w_corr, 0, np.nan)))
-
-    w_in = np.ones(len_wf, dtype=np.int32)
-    w_corr = np.zeros(len_corr, dtype=np.float32)
-    assert np.all(np.isnan(compare_numba_vs_python(wf_correction, w_in, w_corr, 0, -1)))
-
-    w_in = np.ones(len_wf, dtype=np.int32)
-    w_corr = np.zeros(len_corr, dtype=np.float32)
-    assert np.all(np.isnan(compare_numba_vs_python(wf_correction, w_in, w_corr, 0, 9)))
+    # tests on stop_idx
+    with pytest.raises(DSPFatal):
+        compare_numba_vs_python(wf_correction, w_in, w_corr, 0, -1)[-1]
+    with pytest.raises(DSPFatal):
+        compare_numba_vs_python(wf_correction, w_in, w_corr, 0, 9)[-1]
 
     # Case 9: start_idx is larger then stop_idx
-    w_in = np.ones(len_wf, dtype=np.int32)
-    w_corr = np.zeros(len_corr, dtype=np.float32)
-    assert np.all(np.isnan(compare_numba_vs_python(wf_correction, w_in, w_corr, 3, 2)))
+    with pytest.raises(DSPFatal):
+        compare_numba_vs_python(wf_correction, w_in, w_corr, 3, 2)[-1]
 
     # Case 10: stop_idx - start_idx is larger then w_corr length
-    w_in = np.ones(len_wf, dtype=np.int32)
-    w_corr = np.zeros(len_corr, dtype=np.float32)
-    assert np.all(np.isnan(compare_numba_vs_python(wf_correction, w_in, w_corr, 0, 4)))
+    with pytest.raises(DSPFatal):
+        compare_numba_vs_python(wf_correction, w_in, w_corr, 0, 5)[-1]
